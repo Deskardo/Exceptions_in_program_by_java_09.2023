e0680d0a26bc8246cae2d232d0b1a9dd853965ba
Deskardo <kalugin.aleksei.v@gmail.com>
2023-10-03 09:12:25 +0300

    home_work_3_final_work

--- home_work_3_final_work/human_library/client/Program.java
+++ home_work_3_final_work/human_library/client/Program.java
@@ -2,6 +2,10 @@ package home_work_3_final_work.human_library.client;
 
 import home_work_3_final_work.human_library.ui.App;
 
+/**
+ * запуск программы, не забудьте указать путь сохранения данных
+ * файл App.java папка ui
+ */
 public class Program {
     public static void main(String[] args) {
         App app = new App();
